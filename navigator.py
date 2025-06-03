a2115f41ca42f9f716d7049ad7f46d241aac88e5
Alexander <129003161+MrShrimp5566@users.noreply.github.com>
2025-06-03 17:54:56 -0500

    Some functions set in nav class

--- navigator.py
+++ navigator.py
@@ -10,3 +10,36 @@ def list_dir(self):
     except PermissionError:
         return ["[Acess Denied]"]
 
+def change_dir(self, folder_name):
+    new_path = os.path.join(self.current_path, folder_name)
+    if os.path.isdir(new_path):
+        self.history.append(self.currnet_path)
+        self.current_path = new_path
+    else:
+        print("Invalid directory.")
+
+def go_back(self):
+    if self.history:
+        self.current_path = self.history.pop()
+    else:
+        print("Already at root.")
+
+def show_tree(self, path=None, depth=0):
+    path = path or self.current_path
+    if depth > 3: return
+    try:
+        for entry in os.listdir(path):
+            full_path = os.path.join(path,entry)
+            print(" " * depth + "├── " + entry)
+            if os.path.isdir(full_path):
+                self.show_tree(full_path, depth + 1)
+    except PermissionError:
+        print(" " * depth + "└── [Access Denied]")
+
+def search(self, query):
+    matches = []
+    for dirpath, _, filenames in os.walk(self.current_path):
+        for file in filenames:
+            if query.lower() and file.lower()
+                matches.append(os.path.join(dirpath, file))
+    return matches
